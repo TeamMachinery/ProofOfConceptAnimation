c0b9a34c91ba7261dad832f12d556d828da35ff3
Zoran Plesko <zoran.plesko@masinerija.com>
2017-07-14 10:36:06 +0200

    Initial Commit

---
+++ PoCShareResultsAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PoCShareResultsAnimation
+//
+//  Created by Zoran Plesko on 14/07/2017.
+//  Copyright © 2017 Masinerija. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
